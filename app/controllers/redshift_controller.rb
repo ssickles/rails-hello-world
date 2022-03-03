7575de7c1cba62427a7d7f93794d62085d9b471f
Scott Sickles <scottsickles@gmail.com>
2022-03-03 17:38:14 -0500

    remove redshift_url

--- app/controllers/redshift_controller.rb
+++ app/controllers/redshift_controller.rb
@@ -2,7 +2,6 @@ require 'redshift/client'
 
 class RedshiftController < ApplicationController
   def show
-    ENV['REDSHIFT_URL'] = 'redshift://purple_ferret:>EwlMXr:5uClSVO1@purple-ferret-eromt.coqw6amimicq.us-east-1.redshift.amazonaws.com:5439/redshift'
     Redshift::Client.establish_connection
     @redshift_date = Redshift::Client.connection.exec("SELECT GETDATE()").first
   rescue StandardError => e
