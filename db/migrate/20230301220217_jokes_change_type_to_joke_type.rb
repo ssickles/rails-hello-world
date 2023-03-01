f86758640e40e681c252bf0d5d3e77440bfc83d7
Scott Sickles <scottsickles@gmail.com>
2023-03-01 17:03:10 -0500

    forgot to update the column in the migration

--- db/migrate/20230301220217_jokes_change_type_to_joke_type.rb
+++ db/migrate/20230301220217_jokes_change_type_to_joke_type.rb
@@ -1,4 +1,4 @@
-class JokesChangeTypeToJokeType < ActiveRecord::Migration[7.0]
+class JokesChangeTypeToJokeType2 < ActiveRecord::Migration[7.0]
   def change
     rename_column :jokes, :type, :joke_type
   end
