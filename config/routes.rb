c7aaca1429915d7239b5f76a2215504cbcea40b4
Scott Sickles <scottsickles@gmail.com>
2023-03-01 17:11:11 -0500

    add jokes route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :documents, only: [:index, :create]
   resources :logs, only: [:index, :create]
   resource  :redshift, controller: :redshift, only: [:show]
+  resources :jokes, only: [:index]
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
